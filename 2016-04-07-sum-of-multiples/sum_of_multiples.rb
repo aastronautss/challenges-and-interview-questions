29a323ab3f54046fc2977de1c16a22cc3211b09d
Tyler Guillen <tyguillen@gmail.com>
2016-04-08 11:12:49 -0400

    refactor sum_of_multiples

--- 2016-04-07-sum-of-multiples/sum_of_multiples.rb
+++ 2016-04-07-sum-of-multiples/sum_of_multiples.rb
@@ -1,15 +1,15 @@
-require 'pry'
-
 class SumOfMultiples
   def initialize(a = 3, b = 5, *ary)
     ary += [a, b]
-    @nums = ary
+    @tracker = Hash[ary.map { |num| [num, 0] }]
   end
 
   def to(max)
-    (1...max).inject(0) do |sum, num|
-      multiple?(num) ? sum + num : sum
+    sum = 0
+    while incrementors_under? max
+      sum += next_multiple!
     end
+    sum
   end
 
   def self.to(max)
@@ -18,7 +18,15 @@ class SumOfMultiples
 
   private
 
-  def multiple? potential_multiple
-    @nums.any? { |num| potential_multiple % num == 0 }
+  def incrementors_under?(max)
+    @tracker.values.any? { |val| val < max }
+  end
+
+  def next_multiple!
+    lowest = @tracker.values.min
+    @tracker.each do |key, multiple|
+      @tracker[key] += key if multiple == lowest
+    end
+    lowest
   end
 end
